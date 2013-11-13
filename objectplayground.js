752c4f4a7e91a3aab6e6e35d98582b144331c140
noj33 <jonfriese33@gmail.com>
2013-11-12 22:54:53 -0800

    first commit

---
+++ objectplayground.js
@@ -0,0 +1,20 @@
+function TimberPatch(type, boardFeet, manhours, employees) = {
+  this.type = type;
+  this.boardFeet = boardFeet;
+  this.manhours = time;
+  this.employeers = employees;
+
+  //scrape web for currentMarketRate
+  var typeValue = type * currentMarketRate;
+  var laborCost = (manhours * 30);
+  var fuel      = (manhours/employees) * 20;
+
+  this.value = function() {
+    return ((typeValue * boardFeet) - laborCost)
+  };
+
+  this.toString = function () {
+    return "This timber patch has " this.boardFeet " board feet of " + this.type + " and will take " + this.time " hours to harvest";
+  };
+}
+
